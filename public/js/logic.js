d12bd69fd86c12a98b0a8ed439831a2ad4cc9862
Pat Ev <everman.patrick@gmail.com>
2016-10-09 15:46:15 -0400

    cleaned up function logic

--- public/js/logic.js
+++ public/js/logic.js
@@ -7,76 +7,76 @@
         "knees" : {
             "arr" : [
             "knees",
-            "dat knee, disney.JPG",
-            "snap chat.JPG"
+            ["1.JPG","dat knee, disney"],
+            ["2.JPG","snap chat"]
             ]
         },
         "leftArm" : {
             "arr" : [
                 "leftArm",
-                "anime girl, shy guy on telephone.JPG",
-                "big man, caroline david drawing, dice of life.JPG",
-                "boy with exposed belly, ysl, alien workshop logo.JPG",
-                "chino, crying mickey, angelina jolie's dragon tattoo, richie rich, text bubbles, alien workshop logo, smile now cry later.JPG",
-                "chino, crying mickey, angelina jolie's dragon tattoo, richie rich, text bubbles, alien workshop tattoo 2.JPG",
-                "chino, crying mickey, angelina jolie's dragon tattoo, richie rich, text bubbles, alien workshop tattoo.JPG",
-                "douzo yoroshiku with handshake, nothing, .JPG",
-                "linus, nothing.JPG",
-                "mac finder logo, spongebob succ meme.JPG",
-                "rolls royce logo, angry tweety, womb to the tomb, rip shane.JPG",
-                "sensitive thug, no fear eyes, suicidal fish, 'more problems, more money' in russian.JPG",
-                "womb to the tomb, rip shane with teardrop, aesthetics logo, big man, trust, dice of life.JPG"
+                ["1.JPG","anime girl, shy guy on telephone"],
+                ["2.JPG","big man, caroline david drawing, dice of life"],
+                ["3.JPG","boy with exposed belly, ysl, alien workshop logo"],
+                ["4.JPG","chino, crying mickey, angelina jolie's dragon tattoo, richie rich, text bubbles, alien workshop logo, smile now cry later"],
+                ["5.JPG","chino, crying mickey, angelina jolie's dragon tattoo, richie rich, text bubbles, alien workshop tattoo 2"],
+                ["6.JPG","chino, crying mickey, angelina jolie's dragon tattoo, richie rich, text bubbles, alien workshop tattoo"],
+                ["7.JPG","douzo yoroshiku with handshake, nothing"],
+                ["8.JPG","linus, nothing"],
+                ["9.JPG","mac finder logo, spongebob succ meme"],
+                ["10.JPG","rolls royce logo, angry tweety, womb to the tomb, rip shane"],
+                ["11.JPG","sensitive thug, no fear eyes, suicidal fish, 'more problems, more money' in russian"],
+                ["12.JPG","womb to the tomb, rip shane with teardrop, aesthetics logo, big man, trust, dice of life"]
             ]
         },
         "leftLeg" : {
             "arr" : [
                 "leftLeg",
-                "akachan, anime girl 1.JPG",
-                "puppet, praying calvin.JPG"
+                ["1.JPG","akachan, anime girl"],
+                ["2.JPG","puppet, praying calvin"],
             ]
         },
         "leftThigh" : {
             "arr" : [
                 "leftThigh",
-                "'baby' in rose, johnsons logo, 'doggy' in hebrew.JPG",
-                "'im gonna kill you ray romano, anime girl 3, 'baby' in rose, johnson's baby, 'doggy' in hebrew, arnold, greek heads.JPG",
-                "'im gonna kill you ray romano, anime girl 3'.JPG",
-                "'im gonna kill you ray romano', anime girl 3, 'doggy' in hebrew, arnold, johnsons logo, anime girl 2.JPG",
-                "crying daisy duck.JPG",
-                "crying daisy, arnold, anime girl 2.JPG"
+                ["1.JPG","'baby' in rose, johnsons logo, 'doggy' in hebrew"],
+                ["2.JPG","'im gonna kill you ray romano, anime girl 3, 'baby' in rose, johnson's baby, 'doggy' in hebrew, arnold, greek heads"],
+                ["3.JPG","'im gonna kill you ray romano, anime girl 3'"],
+                ["4.JPG","'im gonna kill you ray romano', anime girl 3, 'doggy' in hebrew, arnold, johnsons logo, anime girl 2"],
+                ["5.JPG","crying daisy duck"],
+                ["6.JPG","crying daisy, arnold, anime girl 2"]
             ]
         },
         "rightArm" : {
             "arr" : [
                 "rightArm",
-                "be true, sad clown, girl with armleg, toy machine mascot, 'promise' in russian, empty favicon, godfather logo, face, guardian angel, 456 dice, plug.JPG",
-                "crying minnie, guardian angel, godfather logo.JPG",
-                "godfather logo, 'forever mañana, girl escaping window, wizard staffs.JPG",
-                "jerry, mickey gloves, will dayer drawing, girl in shorts climbing through window, sad devil.JPG",
-                "jerry, mickey gloves, will dyer drawing, girl escaping window, godfather logo, forever mañana.JPG",
-                "jerry, mickey gloves, will dyer drawing, girl with armleg, 'promise' in russian, godfather logo, empty favicon, guardian angel, forever mañana, girl escaping window.JPG",
-                "sad clown, girl with armleg, toy machine logo, empty favicon, 'promise' in russian, face.JPG",
-                "sad clown, hello kitty, plug, ralph lauren logo, foundation moon, 456 dice, guardian angel, face.JPG",
-                "sad devil, 'appropriation' in russian, hotel browanda.JPG",
-                "suicidal daisy duck, 'god' in sign language 2.JPG",
-                "suicidal daisy duck, 'god' in sign language, king lion.JPG",
-                "suicidal daisy duck, 'god' in sign language.JPG"
+                ["1.JPG","be true, sad clown, girl with armleg, toy machine mascot, 'promise' in russian, empty favicon, godfather logo, face, guardian angel, 456 dice, plug"],
+                ["2.JPG","crying minnie, guardian angel, godfather logo"],
+                ["3.JPG","godfather logo, 'forever mañana, girl escaping window, wizard staffs"],
+                ["4.JPG","jerry, mickey gloves, will dayer drawing, girl in shorts climbing through window, sad devil"],
+                ["5.JPG","jerry, mickey gloves, will dyer drawing, girl escaping window, godfather logo, forever mañana"],
+                ["6.JPG","jerry, mickey gloves, will dyer drawing, girl with armleg, 'promise' in russian, godfather logo, empty favicon, guardian angel, forever mañana, girl escaping window"],
+                ["7.JPG","sad clown, girl with armleg, toy machine logo, empty favicon, 'promise' in russian, face"],
+                ["8.JPG","sad clown, hello kitty, plug, ralph lauren logo, foundation moon, 456 dice, guardian angel, face"],
+                ["9.JPG","sad devil, 'appropriation' in russian, hotel browanda"],
+                ["10.JPG","suicidal daisy duck, 'god' in sign language 2"],
+                ["11.JPG","suicidal daisy duck, 'god' in sign language, king lion"],
+                ["12.JPG","suicidal daisy duck, 'god' in sign language"]
             ]
         },
         "rightLeg" : {
             "arr" : [
                 "rightLeg",
-                "509, scream mask, homer with gun, anime girl 6.JPG",
-                "smile now cry later 2, matisse's dance with 'never fake it' caption.JPG",
-                "smile now cry later 2, matisse's dance with 'never fake it' in cursive.JPG"
+                ["1.JPG","509, scream mask, homer with gun, anime girl 6"],
+                ["2.JPG","smile now cry later 2, matisse's dance with 'never fake it' caption"],
+                ["3.JPG","smile now cry later 2, matisse's dance with 'never fake it' in cursive"]
             ]
         },
         "rightThigh" : {
             "arr" : [
                 "rightThigh",
-                "deteriorating bugs bunny, 'regret' in japanse, angela doll, symmetrical faces, anime girl 8, 'twitter' in arabic, 'jumpman', keith haring rendition of icp logo.JPG",
-                "deteriorating bugs bunny, 'regret' in kanji, angela doll, symmetrical faces, anime girl 7, 'twitter' in arabic.JPG",
-                "deteriorating bugs, regret in japanese.JPG"
+                ["1.JPG","deteriorating bugs bunny, 'regret' in japanse, angela doll, symmetrical faces, anime girl 8, 'twitter' in arabic, 'jumpman', keith haring rendition of icp logo"],
+                ["2.JPG","deteriorating bugs bunny, 'regret' in kanji, angela doll, symmetrical faces, anime girl 7, 'twitter' in arabic"],
+                ["3.JPG","deteriorating bugs, regret in japanese"]
             ]
         }
     }
@@ -96,18 +96,21 @@
 
         // called when link is clicked 
         window.render = function(folderPath) { // a key
+
+            //get general arr of clicked category
             var selectedFolder = window.paths[folderPath].arr;
 
-            // assign path for string
+            // get name
             window.stringPath = selectedFolder[0];
 
             let markup = ''
 
-            // iterate through filenames, convert to html, push to string
+            // iterate through arrays after title, index 0 = filename, 1 = caption string
             for (i = 1; i < selectedFolder.length; i++) {
-                var imageHtml = pushPaths(selectedFolder[i]);
+                //add filename
+                var imageHtml = pushPaths(selectedFolder[i][0]);
                 
-                let textString = selectedFolder[i].toString().split('.')[0];
+                let textString = selectedFolder[i][1];
                 let brokenText = textString.split(',');
                 let textHtml = ''
                 for(x=0; x < brokenText.length; x++) {
@@ -130,7 +133,7 @@
         }
 
         window.displayText = function() {
-            $('#content-zone').html('<img class="image top" src="public/Images/rightThigh/deteriorating bugs, regret in japanese.JPG"/>'); //empty footer area
+            $('#content-zone').html('<img class="image top" src="public/Images/rightThigh/1.JPG"/>'); //empty footer area
         }
         
         displayText();
